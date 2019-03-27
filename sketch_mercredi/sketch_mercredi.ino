d722e93decafb80d722018d1492982208199964e
Mathieu <mathieu.neveu@viacesi.fr>
2019-03-27 11:07:42 +0100

    Initial Commit Mathieu

---
+++ sketch_mercredi/sketch_mercredi.ino
@@ -0,0 +1,53 @@
+#include "addi_meteo.h"
+void setup() {
+  // put your setup code here, to run once:
+  lcd.init();
+  lcd.init();
+  lcd.backlight();
+  //Show title splash screen
+  while(millis() < splashScreenDur){
+    lcd.setCursor(3,0);
+    lcd.print(title); 
+  }
+  while(millis() < 10000){
+    //error managing
+    if(si7021.initialize()){
+      lcd.init();
+      lcd.setCursor(1,0);
+      lcd.print("My sensor works!");
+    }else{
+      lcd.init();
+      lcd.setCursor(1,0);
+      lcd.print("No sensor..:(");
+      while(1) {};
+    }
+  }
+  lcd.init();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  //Get values from sensor
+  si7021.getHumidity(hum);
+  si7021.getTemperature(temp);
+  si7021.triggerMeasurement();
+
+  //print values
+  lcd.setCursor(0,0);
+  lcd.print("temp:     ");
+  lcd.print(temp);
+  //print ° sign
+  lcd.createChar(0,degree);
+  lcd.setCursor(14,0);
+// need to re-position after createChar
+  lcd.write(0);
+  lcd.setCursor(15,0);
+  lcd.print("C");
+  
+  lcd.setCursor(0,1);
+  lcd.print("humidit:  ");
+  lcd.print(hum);
+  lcd.print("%");
+
+  lcd.init();
+  }
